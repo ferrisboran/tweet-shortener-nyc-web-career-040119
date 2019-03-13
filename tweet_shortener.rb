67905ab6c6e97e989d69de9f20b8fcd259a26ce8
Ferris Boran <ferris.boran@gmail.com>
2019-03-13 19:23:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,3 +39,11 @@ def bulk_tweet_shortener(array)
     puts word_substituter(word)
   end
 end
+
+def selective_tweet_shortener(string)
+  if string.length > 140
+    word_substituter(string)
+  else
+    string
+  end
+end
