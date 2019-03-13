c6e663a59f3ea62569ae7b1717dc471279c80d8b
Ferris Boran <ferris.boran@gmail.com>
2019-03-13 17:47:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def dictionary
+  replace = {
+      "hello" => "hi",
+      "to" => "2",
+      "two" => "2",
+      "too" => "2",
+      "be" => "b",
+      "you" => "u",
+      "at" => "@",
+      "and" => "&"
+    }
+end
+
+def word_substituter(string)
+  arr = string.split(" ")
+  arr.each do |word|
+    if dictionary.keys.include?(word)
+      arr.replace(dictionary.values)
+    end
+  end
+  arr
+end
